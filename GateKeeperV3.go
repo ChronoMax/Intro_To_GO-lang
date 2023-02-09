da928516e216dce8e55660bcbaf09dd8919722ef
Max Struijs <maxstruys32@gmail.com>
2023-02-09 16:11:28 +0100

    Declaring the licenseplates

---
+++ GateKeeperV3.go
@@ -0,0 +1,20 @@
+package main
+
+//Give input
+func main() {
+	//Declaring the license plates
+	var Licenseplates [3]string
+	Licenseplates[0] = "32-HVT-1"
+	Licenseplates[1] = "45-VET-4"
+	Licenseplates[2] = "09-RQL-9"
+}
+
+//Check input
+func CheckLicenseplates() {
+
+}
+
+//Give back result
+func GiveInputToUser() {
+
+}
