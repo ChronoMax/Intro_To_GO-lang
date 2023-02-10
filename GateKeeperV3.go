642b4614aa732fdbbfeb7ef951f395c9fc9a7473
Max Struijs <maxstruys32@gmail.com>
2023-02-10 16:55:29 +0100

    Added v2 into the script so we can use the greeting message

--- GateKeeperV3.go
+++ GateKeeperV3.go
@@ -1,11 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
 
-//Declaring the license plates
+// Declaring the license plates
 var Licenseplates [3]string
 
-//Give input
+// Give input
 func main() {
 	Licenseplates[0] = "32-H6T-1"
 	Licenseplates[1] = "45-VET-4"
@@ -13,7 +17,7 @@ func main() {
 	CheckLicenseplates()
 }
 
-//Check input
+// Check input
 func CheckLicenseplates() {
 	//Get user input
 	var input string
@@ -25,7 +29,7 @@ func CheckLicenseplates() {
 	GiveInputToUser(input)
 }
 
-//Give back result
+// Give back result
 func GiveInputToUser(_input string) {
 	var result bool = false
 
@@ -40,8 +44,60 @@ func GiveInputToUser(_input string) {
 
 	// printing the final result
 	if result {
-		fmt.Println("Array", Licenseplates, "Contains the given Value", _input)
+		DisplayMessage()
 	} else {
 		fmt.Println("U heeft helaas geen toegang tot het parkeerterrein met kenteken: ", _input)
 	}
+}
+
+func DisplayMessage() {
+	var groet string
+
+	//getting the time. Total, hours and minutes
+	currentTime := time.Now()
+	hour := currentTime.Hour()
+	minutes := currentTime.Minute()
+
+	//adds 0 when the time gets back as single number
+	minuteString := fmt.Sprintf("%02d", minutes)
+
+	//Displaying the time to the user
+	fmt.Println("Current time: ", hour, ":", minuteString)
+
+	//Converting the time to a readable/useable string
+	convertedTime := strconv.Itoa(hour) + minuteString
+
+	//Convert string to int
+	convertedTimeAsInt, err := strconv.Atoi(convertedTime)
+	if err != nil {
+		// ... handle error
+		panic(err)
+	}
+
+	//Printing the converted time [DEBUG ONLY]
+	fmt.Println(convertedTimeAsInt)
+
+	//Setting greeting based on time of day.
+	var defaultText string = "Welkom bij vakantie fonteyn!"
+	var nightText string = "Sorry, de parkeerplaats is ’s nachts gesloten"
+	var textUsed string
+
+	if convertedTimeAsInt >= 700 && convertedTimeAsInt <= 1159 {
+		groet = "Goedemorgen! "
+		textUsed = defaultText
+	}
+	if convertedTimeAsInt >= 1200 && convertedTimeAsInt <= 1759 {
+		groet = "Goedemiddag! "
+		textUsed = defaultText
+	}
+	if convertedTimeAsInt >= 1800 && convertedTimeAsInt <= 2259 {
+		groet = "Goedeavond! "
+		textUsed = defaultText
+	}
+	if convertedTimeAsInt >= 2300 && convertedTimeAsInt <= 659 {
+		groet = ""
+		textUsed = nightText
+	}
+	//Displaying the greeting
+	fmt.Println(groet + textUsed)
 }
